b0a0f00667773d350de04c2a07aedff4e26504f7
MayankCodr <96002289+MayankCodr@users.noreply.github.com>
2022-01-22 11:13:41 +0530

    Registration-Form

---
+++ app.js
@@ -0,0 +1,18 @@
+function checkpwdpat(){
+    var pwd= document.getElementById("pwd").value;
+    var pat=/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*_])[\w!@#$%^&*]{6,}$/;
+    if (pat.test(pwd)==false) {
+        document.getElementById("wpwd").style.display="inline-block";
+        document.getElementById("pwd").value="";
+    } else {
+        document.getElementById("wpwd").style.display="none";
+    }
+}
+
+function reg(){
+    if (document.getElementById("uname").value==""||document.getElementById("email").value==""||document.getElementById("pwd").value=="") {
+        document.getElementById("success").style.display="none";
+    } else {
+        document.getElementById("success").style.display="inline-block";
+    }
+}
